bdae86a89cb05231563ef1f348bf1117bb68c2fe
Aryan <aryanravishankar02@gmail.com>
2020-03-12 19:25:20 -0400

    Commit

--- src/sample/Dictionary.java
+++ src/sample/Dictionary.java
@@ -49,11 +49,6 @@ import java.awt.Font;
 //The slings and arrows of outrageous fortune,
 //Or to take arms against a sea of troubles
 //And by opposing end them. To die—to sleep,
-//No more; and by a sleep to say we end
-//The heart-ache and the thousand natural shocks
-//That flesh is heir to: 'tis a consummation
-//Devoutly to be wish'd. To die, to sleep;
-
 
 public class Dictionary {
 
