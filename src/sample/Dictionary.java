6ab1fb0cd3d25c65cbda0afccb95b0582ecbfe84
Aryan <aryanravishankar02@gmail.com>
2020-03-12 19:25:08 -0400

    Commit

--- src/sample/Dictionary.java
+++ src/sample/Dictionary.java
@@ -53,12 +53,7 @@ import java.awt.Font;
 //The heart-ache and the thousand natural shocks
 //That flesh is heir to: 'tis a consummation
 //Devoutly to be wish'd. To die, to sleep;
-//To sleep, perchance to dream—ay, there's the rub:
-//For in that sleep of death what dreams may come,
-//When we have shuffled off this mortal coil,
-//Must give us pause—there's the respect
-//That makes calamity of so long life.
-//For who would bear the whips and scorns of time,
+
 
 public class Dictionary {
 
