89ad1212a7969d08ef4744bbbc07373462109a0c
Aryan <aryanravishankar02@gmail.com>
2020-03-12 19:25:31 -0400

    Commit

--- src/sample/Dictionary.java
+++ src/sample/Dictionary.java
@@ -44,12 +44,6 @@ import javax.swing.JLabel;
 import java.awt.CardLayout;
 import java.awt.Font;
 
-//To be, or not to be, that is the question:
-//Whether 'tis nobler in the mind to suffer
-//The slings and arrows of outrageous fortune,
-//Or to take arms against a sea of troubles
-//And by opposing end them. To die—to sleep,
-
 public class Dictionary {
 
 	private JFrame frmDictionary;
